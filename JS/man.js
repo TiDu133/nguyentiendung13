6ceda75150dde47ef9c7f91d522666c0bef8bef0
TiDu133 <108079805+TiDu133@users.noreply.github.com>
2022-07-29 18:22:34 +0700

    Add files via upload

---
+++ JS/man.js
@@ -0,0 +1,6 @@
+var typed = new Typed('.element', {
+  strings: ['Designer', 'Designer'],
+  smartBackspace: true,	  loop: true, loopCount: Infinity, backSpeed: 100, typeSpeed: 100,  bindInputFocusEvents: false,
+
+
+});
